71036e0d71622435dad9840b8f5d6ed6de26e4ab
luni64 <lutz.niggl@lunoptics.com>
2021-05-02 13:33:38 +0200

    moved declaration of  pin_t to platform dependent file to allow for different implementations depending on the register set

--- src/platforms/samd.h
+++ src/platforms/samd.h
@@ -1,6 +1,44 @@
 #pragma once
 
 #include "Arduino.h"
+namespace EncoderTool
+{
+    template <typename register_t>
+    class pin_t
+    {
+     public:
+        pin_t(uint_fast8_t pin)
+            : port(digitalPinToPort(pin)),
+              portInputReg(&(port->IN.reg)),
+              portSetReg(&(port->OUTSET.reg)),
+              portClrReg(&(port->OUTCLR.reg)),
+              pinMask(digitalPinToBitMask(pin))
+        {
+
+        }
+
+        void set() const { *portSetReg |= pinMask; }
+        void clear() const { *portClrReg |= pinMask; }
+        uint_fast8_t get() const { return (*portInputReg & pinMask) != 0 ; }
+
+     protected:
+        PortGroup* const port;
+        const volatile register_t* const portInputReg;
+        volatile register_t* const portSetReg;
+        volatile register_t* const portClrReg;
+        const register_t pinMask;
+    };
+}
+
+
+
+
+
+
+
+
+
+#include "Arduino.h"
 
 inline void d_write(uint32_t pin, uint32_t val)
 {
