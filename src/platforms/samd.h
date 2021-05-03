8286def40dc079ca948a59f8d561c121461a4027
luni64 <lutz.niggl@lunoptics.com>
2021-05-03 20:19:59 +0200

    Using a dedicated pin class for each platform -> templates don't make sense anymore.

--- src/platforms/samd.h
+++ src/platforms/samd.h
@@ -3,58 +3,46 @@
 #include "Arduino.h"
 namespace EncoderTool
 {
-    template <typename register_t>
-    class pin_t
+    class Pin
     {
      public:
-        pin_t(uint_fast8_t pin)
+        Pin(uint_fast8_t pin)
             : port(digitalPinToPort(pin)),
               portInputReg(&(port->IN.reg)),
               portSetReg(&(port->OUTSET.reg)),
               portClrReg(&(port->OUTCLR.reg)),
               pinMask(digitalPinToBitMask(pin))
-        {
-
-        }
+        {}
 
         void set() const { *portSetReg |= pinMask; }
         void clear() const { *portClrReg |= pinMask; }
-        uint_fast8_t get() const { return (*portInputReg & pinMask) != 0 ; }
+        uint_fast8_t get() const { return (*portInputReg & pinMask) != 0; }
 
      protected:
         PortGroup* const port;
-        const volatile register_t* const portInputReg;
-        volatile register_t* const portSetReg;
-        volatile register_t* const portClrReg;
-        const register_t pinMask;
+        const volatile uint32_t* const portInputReg;
+        volatile uint32_t* const portSetReg;
+        volatile uint32_t* const portClrReg;
+        const uint32_t pinMask;
     };
 }
 
 
 
-
-
-
-
-
-
-#include "Arduino.h"
-
 inline void d_write(uint32_t pin, uint32_t val)
 {
-  digitalWrite(pin, val);
+    digitalWrite(pin, val);
 }
 
 inline int d_read(uint32_t pin)
 {
-  return digitalRead(pin);
+    return digitalRead(pin);
 }
 
 #if defined(__SAMD51__)
 
-#define PLAIN_ENC_CALLBACK
+#    define PLAIN_ENC_CALLBACK
 
 #else
-
 
 #endif
