211e213a5fcf5f201590aec1607438e176d52032
luni64 <lutz.niggl@lunoptics.com>
2021-05-08 11:07:24 +0200

    Remove gnu++14 dependence from attachInterruptEx since most of  non teensy boards still compile for gnu++11

--- src/Single/attachInterruptEx.cpp
+++ src/Single/attachInterruptEx.cpp
@@ -1,41 +1,87 @@
-#include <array>
-#include <utility>
 #include "attachInterruptEx.h"
-#include "../config.h"
+//#include "../config.h"
+#include "Arduino.h"
 
-
-using namespace std;
 
 namespace EncoderTool
 {
-    // We need to attach a memberfunction of the encoder class to a pin interrupt.
-    // Since attachInterrupt only accepts state less void(*)(void) callbacks this is not possible directly.
-    // Instead,we generate an array of void(*)() relay functions to attach to the pin interrupts. Those relays will invoke the actual callbacks.
-    // see https://stackoverflow.com/q/63730007/1842762
+    constexpr unsigned N = 4;
 
-    // ! We don't use <std::function> here (EncoderTool) to allow usage from smaller MCUs like TeensyLC !
-
-    cb_t callbacks[NUM_DIGITAL_PINS]; // storage for the callback functions
-    state_t states[NUM_DIGITAL_PINS]; // storage for the corresponding state variables
-
-    template <unsigned nr>
-    constexpr void relay()
+    namespace // private
     {
-        callbacks[nr](states[nr]);
+        using relay_t = void (*)();
+
+        callback_t callbacks[N]; // pointers to callback functions
+        Encoder* states[N];      // state passed to callback functions
+        relay_t relays[N];       // pointers to functions relaying calls from pin interrupts to the callback functions
+        uint8_t pins[N];         // keep track which pin is attached to which callback
+
+        // Functions to be attached to the pin interrupts. They relay the ISR to the actual callback
+        template <unsigned nr>
+        void relay()
+        {
+            callbacks[nr](states[nr]);
+        }
+
+        // intialize the array of relay functions recursively
+        template <size_t N>  __attribute__((always_inline))
+        inline void initRelays()
+        {
+            pins[N]   = UINT8_MAX;
+            relays[N] = relay<N>;
+            initRelays<N - 1>();
+        }
+
+        template <> __attribute__((always_inline))
+        inline void initRelays<0>()
+        {
+            pins[0]   = UINT8_MAX;
+            relays[0] = relay<0>;
+        }
+
+        bool initialized = false;
+        void initialize()
+        {
+            Serial.println("initialize");
+            initRelays<N - 1>();
+            initialized = true;
+        }
+
+    } // private namespace
+
+    bool attachInterruptEx(unsigned pin, callback_t callback, Encoder* state, int mode)
+    {
+        Serial.printf("attach %d\n", pin);
+        if (!initialized) initialize();
+
+        for (unsigned i = 0; i < N; i++)
+        {
+            if (pins[i] == UINT8_MAX || pins[i] == pin)
+            {
+                pins[i]      = pin;
+                callbacks[i] = callback;
+                states[i]    = state;
+                attachInterrupt(pin, relays[i], mode);
+                Serial.printf("ch: %d OK\n", i);
+                return true;
+            }
+        }
+        Serial.printf("ERR\n");
+        return false;
     }
 
-    template <unsigned... nr>
-    constexpr array<void (*)(), NUM_DIGITAL_PINS> MakeRelays(index_sequence<nr...>)
+    bool detachInterruptEx(unsigned pin)
     {
-        return array<void (*)(), NUM_DIGITAL_PINS>{relay<nr>...};
-    }
+        detachInterrupt(pin);
 
-    constexpr auto relays = MakeRelays(make_index_sequence<NUM_DIGITAL_PINS>{}); // generates the relay function array at compile time
-
-    void attachInterruptEx(unsigned pin, cb_t callback, state_t state, int mode)
-    {
-        states[pin]    = state;
-        callbacks[pin] = callback;
-        attachInterrupt(pin, relays[pin], mode);
+        for (unsigned i = 0; i < N; i++)
+        {
+            if (pins[i] == pin)
+            {
+                pins[i] = UINT8_MAX;
+                return true;
+            }
+        }
+        return false;
     }
 }
--- src/Single/attachInterruptEx.h
+++ src/Single/attachInterruptEx.h
@@ -3,9 +3,8 @@
 namespace EncoderTool
 {
     class Encoder;
+    using callback_t = void (*)(Encoder* state); // callback type
 
-    using state_t = Encoder*;             // we directly use an Encoder* as state variable to avoid casts from void* to Encoder* in the ISR
-    using cb_t = void (*)(state_t state); // callback type
-
-    void attachInterruptEx(unsigned pin, cb_t callback, state_t state, int mode);
+    bool attachInterruptEx(unsigned pin, callback_t callback, Encoder* state, int mode);
+    bool detachInterruptEx(unsigned pin);
 }
